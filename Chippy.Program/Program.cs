3820b3dbeca760f75b8425ddc52d393c74ce4070
Thomas-Fortier <thomasfortier.cs@gmail.com>
2023-06-20 00:44:47 -0400

    Added basic window

--- Chippy.Program/Program.cs
+++ Chippy.Program/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Chippy.Program
+﻿using Chippy.Program;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace Chippy.Programe
 {
   internal class Program
   {
-    static void Main(string[] args)
+    private static void Main()
     {
-      Console.WriteLine("Hello, World!");
+      var window = CreateWindow();
+      window.Run();
+    }
+
+    private static Window CreateWindow()
+    {
+      var gameSettings = new GameWindowSettings
+      {
+        UpdateFrequency = 600
+      };
+
+      var nativeSettings = new NativeWindowSettings
+      {
+        Size = new Vector2i(1024, 512),
+        Profile = ContextProfile.Compatability,
+        Title = "Chippy"
+      };
+
+      const int NATIVE_WIDTH = 64;
+      const int NATIVE_HEIGHT = 32;
+
+      return new Window(gameSettings, nativeSettings, NATIVE_WIDTH, NATIVE_HEIGHT);
     }
   }
 }
---
+++ Chippy.Program/Window.cs
@@ -0,0 +1,66 @@
+﻿using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System.Drawing;
+
+namespace Chippy.Program
+{
+  internal class Window : GameWindow
+  {
+    private int _nativeWidth = 64;
+    private int _nativeHeight = 32;
+
+    public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings, int nativeWidth, int nativeHeight)
+      : base(gameWindowSettings, nativeWindowSettings)
+    {
+      _nativeWidth = nativeWidth;
+      _nativeHeight = nativeHeight;
+    }
+
+    protected override void OnLoad()
+    {
+      base.OnLoad();
+
+      GL.ClearColor(Color.Black);
+      GL.Color3(Color.White);
+      GL.Ortho(0, 64, 32, 0, -1, 1);
+      GL.Clear(ClearBufferMask.ColorBufferBit);
+
+      SwapBuffers();
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs e)
+    {
+      base.OnRenderFrame(e);
+
+      GL.Clear(ClearBufferMask.ColorBufferBit);
+
+      SwapBuffers();
+    }
+
+    protected override void OnResize(ResizeEventArgs e)
+    {
+      base.OnResize(e);
+
+      GL.Viewport(0, 0, e.Width, e.Height);
+    }
+
+    public void Render(byte[] buffer)
+    {
+      GL.Clear(ClearBufferMask.ColorBufferBit);
+
+      for (int y = 0; y < _nativeHeight; y++)
+      {
+        for (int x = 0; x < _nativeWidth; x++)
+        {
+          var currentPixel = buffer[y * _nativeWidth + x];
+
+          if (currentPixel > 0)
+          {
+            GL.Rect(x, y, x + 1, y + 1);
+          }
+        }
+      }
+    }
+  }
+}
